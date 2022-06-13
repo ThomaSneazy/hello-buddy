206f5d20faa0a3d7cbc24709f00b764810ad1ee3
Thomas Fouquert <tfouquert@gmail.com>
2022-06-13 15:19:07 +0200

    add_cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name address phone description birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name address phone description birth_date photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name address phone description birth_date])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name address phone description birth_date photo])
   end
 end
