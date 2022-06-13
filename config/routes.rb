3b394e13e78956a9eda613de03bc9385b5157acf
Thomas Fouquert <tfouquert@gmail.com>
2022-06-13 15:35:17 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to:  'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :activities do
+    resources :bookings, only: %i[new create edit update]
+  end
+  resources :bookings, only: [:destroy]
 end
