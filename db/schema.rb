50d2e747ddf9ca20950d6cf4b63d321137e2d6ec
Thomas Fouquert <tfouquert@gmail.com>
2022-06-13 16:27:48 +0200

    change_type_max_participant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_13_131535) do
+ActiveRecord::Schema.define(version: 2022_06_13_134017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,11 +51,11 @@ ActiveRecord::Schema.define(version: 2022_06_13_131535) do
     t.text "location"
     t.string "meeting_point"
     t.integer "difficulty"
-    t.string "max_of_participant"
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "max_of_participant"
     t.index ["category_id"], name: "index_activities_on_category_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
