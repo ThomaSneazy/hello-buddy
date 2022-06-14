5047432b1ca18a6b759f977221259ae781d6a96b
fatoudiallo <fatous.diallo@yahoo.fr>
2022-06-14 16:52:55 +0200

    profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,4 @@
+class ProfilesController < ApplicationController
+  def profiles
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:  'categories#index'
+  get 'profiles', to: 'profiles#profiles', as: :profiles
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routin, .html
   resources :categories do
     resources :activities do
