d071c13be85aff142b8b379baba59a833ac2bed2
thomascarre <carrethomas7@gmail.com>
2022-06-14 17:40:48 +0200

    add view activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,8 @@
 class Activity < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
+  validates :title, :description, :meeting_point, :start_date, presence: true
+  validates :difficulty, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
