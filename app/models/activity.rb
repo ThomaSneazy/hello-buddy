bdc63be984f41b3064b0f3b95bf7871e10bf5e20
Thomas Fouquert <tfouquert@gmail.com>
2022-06-14 17:42:38 +0200

    start_activity_show

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :bookings
   geocoded_by :meeting_point
   after_validation :geocode, if: :will_save_change_to_meeting_point?
 end
