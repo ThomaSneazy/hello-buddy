41a0030dcdfbe636c269daad4187ebdcb0220721
Thomas Fouquert <tfouquert@gmail.com>
2022-06-15 10:58:54 +0200

    add_material_to_activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_131251) do
+ActiveRecord::Schema.define(version: 2022_06_15_085736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_06_14_131251) do
     t.float "latitude"
     t.float "longitude"
     t.datetime "start_date"
+    t.string "material"
     t.index ["category_id"], name: "index_activities_on_category_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
