95218c7ea8de40fa06b1aa72eb9187ab408268a2
Thomas Fouquert <tfouquert@gmail.com>
2022-06-16 13:59:04 +0200

    second commit

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,6 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  validates :title, :description, :meeting_point, presence: true
+  validates :title, :description, :meeting_point, :start_date, presence: true
   validates :difficulty, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
