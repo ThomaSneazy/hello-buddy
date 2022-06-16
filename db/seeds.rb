596980d0a81a60309dd40b388d7af03871f7e882
thomascarre <carrethomas7@gmail.com>
2022-06-16 15:53:08 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
-
+Booking.destroy_all
 Activity.destroy_all
 Category.destroy_all
 User.destroy_all
