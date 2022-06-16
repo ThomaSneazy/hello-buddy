2a731e3d5789c919d36176facf85e41efec60c8e
Nicolas Ruff <nicoff.ruff@gmail.com>
2022-06-16 14:35:06 +0200

    ajout pg search

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,10 @@ class Category < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
   validates :name, uniqueness: true, presence: true
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: :title,
+    using: {
+      tsearch: { prefix: true }
+    }
 end
