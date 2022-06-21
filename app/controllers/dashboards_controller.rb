fb32c6791a7ea3bcc3b6f6abc6dd4590875346dc
thomascarre <carrethomas7@gmail.com>
2022-06-21 14:55:43 +0200

    add-notif

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,6 +6,5 @@ class DashboardsController < ApplicationController
     end
     @my_activities = Activity.where(user_id: current_user.id)
     @my_participations = Booking.where(user_id: current_user.id)
-
   end
 end
